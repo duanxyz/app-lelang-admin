12e67669c8e33bf6cd034336efa1daa93fb9aa4b
duan <nubitol45@gmail.com>
2020-05-04 21:43:22 +0700

    initialized

---
+++ database/factories/MemberFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Laravue\Models\Member;
+use Faker\Generator as Faker;
+
+$factory->define(Member::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
